78139026a401de9e8e0ed938b4d1eefabb23a6a8
saksham3218 <72651240+saksham3218@users.noreply.github.com>
2020-12-10 15:55:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var  database;
+var gameState = 0;
+var playerCount;
+var form,player,game
+
+function setup(){
+  database = firebase.database();
+  
+  createCanvas(500,500);
+  //game = new Game()
+ // game.getState()
+  //game.start()
+  form = new Form()
+}
+
+function draw(){
+  
+ form.display();
+  
+}
+
